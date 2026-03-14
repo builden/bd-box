98cdc069cca6675788448a39ed25fe86f763e415
builden <billdeng@qq.com>
2026-03-14 12:05:28 +0800

    refactor(server): 添加类型导出占位符
    - server/types.ts: 类型导出入口（占位符）

---
+++ packages/bd-cc/server/types.ts
@@ -0,0 +1,11 @@
+/**
+ * Server Types Barrel Export
+ *
+ * 这个文件作为 server 模块的类型导出入口。
+ * 当需要添加跨模块共享的类型时，在此统一导出。
+ *
+ * Usage:
+ *   import type { SomeType } from './types.js';
+ */
+
+// TODO: 添加跨模块共享的类型定义
