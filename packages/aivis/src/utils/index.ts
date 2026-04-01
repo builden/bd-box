55b0473b6cfe61a23fbd6d308a89039acc80ca3b
builden <billdeng@qq.com>
2026-04-02 04:28:53 +0800

    feat(aivis): replicate utils from agentation

---
+++ packages/aivis/src/utils/index.ts
@@ -0,0 +1,7 @@
+export * from './element-identification';
+export * from './freeze-animations';
+export * from './generate-output';
+export * from './react-detection';
+export * from './source-location';
+export * from './storage';
+export * from './sync';
